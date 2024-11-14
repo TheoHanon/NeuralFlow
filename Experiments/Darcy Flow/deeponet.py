1443f401ae1da6b1df8438d338c786e6bbdf51bc
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2024-11-14 12:27:37 +0100

    Enhance DeepONet: Add x_bounds and t_bounds parameters, implement CNN-based branch network, and update call method to incorporate bounds in output calculation

--- Experiments/Darcy Flow/deeponet.py
+++ Experiments/Darcy Flow/deeponet.py
@@ -4,7 +4,16 @@ from typing import Union
 
 
 class DeepONet(tf.keras.Model):
-    def __init__(self, n_branch:int, n_trunk : int, width:int, depth:int, output_dim:int, activation : Union[str, callable]) -> None:
+    def __init__(self, 
+                 n_branch:int, 
+                 n_trunk : int, 
+                 width:int, 
+                 depth:int, 
+                 output_dim:int, 
+                 activation : Union[str, callable], 
+                 x_bounds : tuple = (0, 1.0),
+                 t_bounds : tuple = (0, 1.0)
+                 ) -> None:
         """
         DeepONet model
         :param n_branch: number of branches
@@ -24,11 +33,19 @@ class DeepONet(tf.keras.Model):
         self.output_dim = output_dim
         self.activation = activation
         
-        self.branch_net = MLP(input_size = self.n_branch, hidden_size = self.width, output_size = self.output_dim, depth = self.depth, activation = self.activation, final_activation=False)
+        self.x_bounds = x_bounds
+        self.t_bounds = t_bounds
+
+
+        self.branch_net = CNNBranchNet2D(input_shape = (self.n_branch, self.n_branch, 1), output_dim = self.output_dim, activation = self.activation)
         self.trunk_net = MLP(input_size = self.n_trunk, hidden_size = self.width, output_size = self.output_dim, depth = self.depth, activation = self.activation, final_activation=True)
 
         self.bias = tf.Variable([1.0])
 
+    def compile(self, optimizer: tf.keras.optimizers.Optimizer, loss_fn) -> None:
+        super(DeepONet, self).compile()
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
 
     def call(self, x_branch: tf.Tensor, x_trunk: tf.Tensor) -> tf.Tensor:
         """
@@ -38,11 +55,44 @@ class DeepONet(tf.keras.Model):
         :return: output tensor
         """
 
-        
+        x, y = x_trunk[..., 0], x_trunk[..., 1]
+        bounds = (x - self.x_bounds[0]) * (x - self.x_bounds[1]) * (y - self.t_bounds[0]) * (y - self.t_bounds[1])
+
         branch_out = self.branch_net(x_branch)
         trunk_out = self.trunk_net(x_trunk)
+        
+        return (tf.reduce_sum(branch_out * trunk_out, axis=1, keepdims=True) + self.bias) * bounds
+        
 
-        return branch_out @ tf.transpose(trunk_out) + self.bias
+    
+
+class CNNBranchNet2D(tf.keras.Model):
+
+    def __init__(self, input_shape: tuple, output_dim: int, activation: Union[str, callable]) -> None:
+        """
+        CNN-based branch network for DeepONet (2D)
+        :param input_shape: shape of the input (2D structure)
+        :param output_dim: output dimension
+        :param activation: activation function
+        """
+        super(CNNBranchNet2D, self).__init__()
+        
+        self.cnn = tf.keras.Sequential([
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation=activation),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation=activation),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(output_dim, activation=None)
+        ])
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """
+        Forward pass for CNN branch
+        :param x: input tensor
+        :return: output tensor
+        """
+        return self.cnn(x)
+
 
 
 
@@ -66,7 +116,7 @@ class MLP(tf.keras.Model):
 
         self.mlp = tf.keras.Sequential()
 
-        self.mlp.add(tf.keras.layers.InputLayer(input_shape=(input_size,)))
+        self.mlp.add(tf.keras.Input(shape=(input_size,)))
         
         for _ in range(depth):
             self.mlp.add(tf.keras.layers.Dense(hidden_size, activation=activation))
